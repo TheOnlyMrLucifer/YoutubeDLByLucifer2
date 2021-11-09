7cde8d570271c1c3b23a358955f510b068a6ee43
Chloe Decker <69217587+TheOnlyMrLucifer@users.noreply.github.com>
2021-11-09 14:50:55 +0100

    V1
    V1

---
+++ README.md
@@ -0,0 +1,36 @@
+## Uploader X  Bot
+---
+# HEROKU DEPLOY
+# [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://www.heroku.com/deploy)
+
+# RAILWAY deploy 
+### DEPLOY ON RAILWAY 🚂 it's working
+[![Deploy+on+Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https://github.com/TheOnlyMrLucifer/YoutubeDLByLucifer2/URL-UPLOAD&envs=TG_BOT_TOKEN,WEBHOOK)
+#
+# `TG_BOT_TOKEN` put your bot token
+# `WEBHOOK` = `ANYTHING`
+
+Telegram RoBot to Upload Links.
+
+**Features**:
+
+👉 Upload [YouTube-dl Supported Links](https://ytdl-org.github.io/youtube-dl/supportedsites.html) to Telegram.
+
+👉 Upload HTTP/HTTPS as File/Video to Telegram.
+
+👉 Upload zee5, sony.live, voot and much more 😋
+
+👉 Broadcast message, check total users
+
+👉  Permanent thumbnail Support
+
+**Heroku Buildpacks**
+```
+https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest
+```
+```
+heroku/python
+```
+
+#### LICENSE
+- GPLv3
---
+++ plugins/help_text.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+from pyrogram import filters
+from database.adduser import AddUser
+from pyrogram import Client as Clinton
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Clinton.on_message(filters.private & filters.command(["help"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    await AddUser(bot, update)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_USER,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+
+@Clinton.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    # logger.info(update)
+    await AddUser(bot, update)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Source code ⚡", url="https://github.com/Clinton-Abraham/UPLOADER-BOT"
+                    ),
+                    InlineKeyboardButton("moded Source code  👨🏻‍💻", url="https://github.com/Rplayoriginal/URL-UPLOAD"),
+                ],
+                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/clinton_abraham_bot")],
+                [InlineKeyboardButton("MOD Developer 👨‍⚖️", url="https://t.me/renishrplay")],
+            [InlineKeyboardButton("VIDEO COMPRESSOR BOT", url="https://t.me/Rplay_compressor_bot")],
+                [InlineKeyboardButton("RENAME BOT", url="https://t.me/Rplay_rename_bot")],
+                [InlineKeyboardButton("VIDEO MERGE BOT", url="https://t.me/Rplay_video_mergebot")]
+            ]
+        ),
+        reply_to_message_id=update.message_id
+    )
